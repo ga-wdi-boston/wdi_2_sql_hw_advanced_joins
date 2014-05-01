74e8af7d72a7a3f658aa0f2a967493812bdbb5fa
Ethan Gould <ethan.g.907@gmail.com>
2014-05-01 19:13:06 -0400

    created query file to store answers

---
+++ query.sql
@@ -0,0 +1,31 @@
+\c blog
+
+-- 1. How many comments has each user made per category?
+
+
+-- 2. Get a listing of all posts grouped by year
+
+
+-- 3. How many comments does each user have across all of their posts?
+
+
+-- 4. Which posts contain a specific keyword?
+
+
+-- 5. Which category of post has each user made the most comments on?
+
+
+-- 6. Get a specific user's posts sorted by date of most recent comment.
+
+
+-- 7. Get all posts by a specific user which that user themselves has *not* commented on, but which have at least one comment.
+
+
+-- 8. Get the top 5 most-commented-on posts that were created in the last 7 days.
+
+
+-- 9. Get the top 5 wordiest posts, in terms of character count.
+
+
+-- 10. Get a listing of all posts sorted by longest-running comment thread (length of time between the first and last comments).
+
