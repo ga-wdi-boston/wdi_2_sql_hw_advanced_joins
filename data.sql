55246da78b87ed6211548a3cc91aa958de094447
Lisa Plesko <plesko.l@gmail.com>
2014-05-02 07:34:41 -0400

    Queries can (maybe) do some more tricks...

--- data.sql
+++ data.sql
@@ -6,7 +6,12 @@ INSERT INTO users (user_name, password)
   ('dfisher', 'pw789'),
   ('agrant', 'pw023'),
   ('kvotolato', 'pw934'),
-  ('egould', 'pw766');
+  ('egould', 'pw766'),
+  ('mtalley', 'pw332'),
+  ('amcdermott', 'pw245'),
+  ('gstraehle', 'pw114'),
+  ('kmccarthy', 'pw119'),
+  ('tdyer', 'pw228');
 
 INSERT INTO categories (name)
   VALUES ('Web Design'),
@@ -22,11 +27,16 @@ WITH first_insert (author, title, created_at, content, category) AS   --if creat
   (VALUES
     ('lplesko', 'Web Dev Programs', '2013-06-01', 'Testing 1-2-3.', 'Programming'),
     ('lplesko', 'Web Dev Programs', '2013-06-10', 'Evaluating the pros and cons of dev bootcamps.', 'Programming'),
-    ('lplesko', 'Web Dev Programs, continued', '2013-07-23', 'After further consideration, I will be joining General Assembly!', 'Programming'),
+    ('kmccarthy', 'Boston-bound', '2013-07-01', 'I am moving to Boston from San Francisco in the spring; any advice?','Miscellaneous'),
+    ('clafontant', 'General Assembly Open House', '2013-07-10', 'On Friday, I attended the WDI open house at General Assembly to learn about their upcoming program.', 'Programming'),
+    ('lplesko', 'Web Dev Programs, continued', '2013-07-23', 'Excited to announce that I will be joining General Assembly in the April 2014 section - time to start my prework.', 'Programming'),
     ('lplesko', 'Twitter Bootstrap', '2014-04-21', 'This is a great layout template.', 'Web Design'),
-    ('lplesko','Database Tips and Tricks', '2014-04-24', 'Content for this blog post.', 'Programming'),
+    ('lplesko','Database Tips and Tricks', '2014-04-24', 'Here is a list of helpful tips I found when working with databases.', 'Programming'),
     ('dfisher','Hackathon this weekend!', '2014-04-27', 'Everyone head to the Internet of Things.', 'Class Happenings'),
-    ('egould','Great time at the Hackathon', '2014-04-28', 'My project won!', 'Class Happenings')
+    ('egould','Great time at the Hackathon', '2014-04-28', 'My project won!', 'Class Happenings'),
+    ('kvotolato','Weekend', '2014-04-28', 'I attended the cardboard game jam, I highly recommend it!', 'Class Happenings'),
+    ('tdyer','Working with parent and child classes', '2014-04-30', 'Below is an exercise with car lots to better demonstrate how to work with classes in Ruby.', 'Programming'),
+    ('dfisher','Waffle recipes', '2014-05-01', 'Anyone have a good recipe for two gallons of waffles?', 'Class Happenings')
     )
 INSERT INTO posts
   (user_id, title, created_at, content, category_id)
@@ -42,12 +52,16 @@ FROM
 WITH second_insert (author, title, created_at, content) AS
   (VALUES
     ('dfisher', 'Web Dev Programs', '2013-06-14', 'Join General Assemblys 12-week WDI program'),
-    ('agrant', 'Web Dev Programs', '2013-06-15', 'I am amazed what students can learn in 3 months what I learned in 3 years.'),
+    ('agrant', 'Web Dev Programs', '2013-06-15', 'I am amazed what students can learn in three months what I learned in three years.'),
+    ('dfisher', 'Boston-bound', '2013-07-02', 'Yes - join General Assembly.'),
     ('kvotolato', 'Web Dev Programs', '2013-09-20', 'Hi Lisa, I will be joining, too!'),
     ('clafontant', 'Twitter Bootstrap', '2014-04-22', 'I look forward to using this for my next layout.'),
     ('agrant','Database Tips and Tricks', '2014-04-26', 'Nobody actually ever does this.'),
+    ('gstraehle','Database Tips and Tricks', '2014-04-27', 'I have a helpful tip: do not set a date to February 30!'),
     ('lplesko','Hackathon this weekend!', '2014-04-27', 'Sounds fun, I plan on attending!'),
-    ('egould','Hackathon this weekend!', '2014-04-29', 'I just picked up my ticket, I can not wait to attend.')
+    ('egould','Hackathon this weekend!', '2014-04-29', 'I just picked up my ticket, I can not wait to attend.'),
+    ('amcdermott','Waffle recipes', '2014-05-01', 'I will bring strawberries!!'),
+    ('tdyer','Waffle recipes', '2014-05-02', 'Gluten, all you need is gluten!')
     )
 INSERT INTO comments
   (user_id, post_id, created_at, content)
--- queries.sql
+++ queries.sql
@@ -1,11 +1,25 @@
 \c blog
 
+-- 1. How many comments has each user made per category?
+
+SELECT users.user_name, categories.name AS category, count(comments.user_id) AS comments_per
+  FROM users, posts, categories, comments
+  WHERE users.id = comments.user_id AND comments.post_id = posts.id AND posts.category_id = categories.id
+  GROUP BY users.user_name, categories.name;
+
 -- 2. Get a listing of all posts grouped by year.
 
 SELECT title, extract(year FROM created_at::DATE) AS year
   FROM posts
   ORDER BY created_at DESC;
 
+
+-- 3. How many comments does each user have across all of their posts?
+
+SELECT DISTINCT users.user_name, posts.title AS post_title, count(posts.id = comments.post_id) AS comments_on_posts
+  FROM posts, users, comments
+  WHERE users.id = posts.user_id AND posts.id = comments.post_id
+  GROUP BY users.user_name, posts.title;
 
 
 
@@ -15,24 +29,46 @@ SELECT title, content FROM posts WHERE content ILIKE '% the %';
 -- SELECT title, content FROM posts WHERE content ~* ' the ';
 
 
+
+-- 5.  Which category of post has each user made the most comments on?
+
+
+
 -- 6.  Get a specific user's posts sorted by date of most recent comment.
 
-SELECT users.user_name, posts.title, comments.content, comments.created_at
+SELECT DISTINCT users.user_name AS post_owner, posts.title, comments.content AS comment, comments.created_at
   FROM posts, users, comments
   WHERE users.id = posts.user_id AND posts.id = comments.post_id
   ORDER BY comments.created_at DESC LIMIT 1;
 
 
+-- 7. Get all posts by a specific user that have comments, but which that user hasn't commented on.
 
--- SELECT users.user_name, posts.title
---   FROM posts, users
---   WHERE users.id = posts.user_id;
+
+
+-- 8. Get the top 5 most-commented-on posts that were created in the last 7 days.
+
+SELECT posts.title, posts.created_at, count(posts.id = comments.post_id) FROM posts
+  LEFT OUTER JOIN comments
+  ON (posts.id = comments.post_id)
+  WHERE (posts.created_at >= current_date - interval '7 days')
+  GROUP BY posts.title, posts.created_at LIMIT 5;
 
 
 -- 9.  Get the top 5 wordiest posts (just by character count – don't actually count words).
 
 --SELECT title, sum(length(content)) AS character_count FROM posts GROUP BY title LIMIT 5;
 SELECT title, sum(length(content)) AS character_count FROM posts GROUP BY title ORDER BY character_count DESC LIMIT 5;
+
+
+-- 10 Get all posts sorted by longest-running comment thread (time between first and last comments).
+
+SELECT posts.title, comments.content, max(comments.created_at)
+  FROM posts, comments
+  WHERE posts.id = comments.post_id
+  GROUP BY posts.title, comments.content;
+
+
 
 
 -- Inner Join
