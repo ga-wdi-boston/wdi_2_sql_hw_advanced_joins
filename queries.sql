7caf474ef8b1022a6770b68bc9e5f331cbf9ecd4
Devin Dreszer <devindreszer@gmail.com>
2014-06-19 16:42:33 -0400

    Create 6 queries

--- queries.sql
+++ queries.sql
@@ -1,23 +1,45 @@
+\c blog
 -- 1. Get posts containing a specific keyword (e.g. "about").
-
+SELECT title FROM posts WHERE content ILIKE '%about%';
 
 -- 2. Get a listing of all posts grouped by year.
-
+SELECT extract(year from created_at), title
+  FROM posts
+  GROUP BY extract(year from created_at), title
+  ORDER BY extract(year from created_at);
 
 -- 3. Get the top 5 wordiest posts by character count.
-
+-- SELECT title, COUNT(content)
+--   FROM posts
+--   ORDER BY COUNT(content) DESC
+--   LIMIT 5;
 
 -- 4. Find how many comments each user has across all of their posts.
-
+SELECT users.login, COUNT(comments.*)
+  FROM users, comments, posts
+  WHERE comments.post_id = posts.id AND posts.author_id = users.id
+  GROUP BY users.login
+  ORDER BY COUNT(comments.*);
 
 -- 5. Get a specific user's posts sorted by date of most recent comment.
-
+SELECT posts.title
+  FROM users, posts, comments
+  WHERE comments.post_id = posts.id AND posts.author_id = users.id AND users.login = 'George'
+  ORDER BY comments.created_at DESC;
 
 -- 6. Find how many comments each user has made per post category.
-
+SELECT users.login, categories.name, COUNT(comments.*)
+  FROM users, comments, posts, categories
+  WHERE comments.author_id = users.id AND comments.post_id = posts.id AND posts.category_id = categories.id
+  GROUP BY users.login, categories.name
+  ORDER BY users.login;
 
 -- 7. Get the 5 most-commented-on posts that were created in the last 7 days.
-
+SELECT posts.title, COUNT(comments.*)
+  FROM posts, comments
+  WHERE comments.post_id = posts.id AND posts.created_at > CURRENT_TIMESTAMP - interval '7 days'
+  GROUP BY posts.title
+  ORDER BY COUNT(comments.*) DESC LIMIT 5;
 
 -- 8. Get the 5 posts with the longest-running comment threads
 --    (longest time between first and last comments).
