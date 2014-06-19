a84926f6a65ec02ecb9a8c71100fbbb046ef090b
Devin Dreszer <devindreszer@gmail.com>
2014-06-19 18:26:44 -0400

    Refactor code to use aliases

--- queries.sql
+++ queries.sql
@@ -3,30 +3,31 @@
 SELECT * FROM posts WHERE content ILIKE '%about%';
 
 -- 2. Get a listing of all posts grouped by year.
-SELECT extract(year from created_at), posts
+SELECT extract(year from created_at) AS year, posts.title
   FROM posts
-  GROUP BY extract(year from created_at), posts
-  ORDER BY extract(year from created_at);
+  ORDER BY year;
 
 -- 3. Get the top 5 wordiest posts by character count.
-SELECT * FROM posts
-  ORDER BY char_length(content) DESC LIMIT 5;
+SELECT title, char_length(content) AS characters
+  FROM posts
+  ORDER BY characters DESC LIMIT 5;
 
 -- 4. Find how many comments each user has across all of their posts.
-SELECT users.login, COUNT(comments.*)
+SELECT users.login, COUNT(comments.*) AS total_comments
   FROM users, comments, posts
   WHERE comments.post_id = posts.id AND posts.author_id = users.id
   GROUP BY users.login
-  ORDER BY COUNT(comments.*);
+  ORDER BY total_comments;
 
 -- 5. Get a specific user's posts sorted by date of most recent comment.
-SELECT posts
+SELECT posts.title, MAX(comments.created_at) AS comments_created_at
   FROM users, posts, comments
-  WHERE comments.post_id = posts.id AND posts.author_id = users.id AND users.login = 'George'
-  ORDER BY comments.created_at DESC;
+  WHERE comments.post_id = posts.id AND posts.author_id = users.id AND users.login = 'Jill'
+  GROUP BY posts.title
+  ORDER BY comments_created_at DESC;
 
 -- 6. Find how many comments each user has made per post category.
-SELECT users.login, categories.name, COUNT(comments.*)
+SELECT users.login, categories.name, COUNT(comments.*) AS comments_count
   FROM users, comments, posts, categories
   WHERE comments.author_id = users.id AND comments.post_id = posts.id AND posts.category_id = categories.id
   GROUP BY users.login, categories.name
@@ -41,20 +42,20 @@ SELECT COUNT(comments.*), posts
 
 -- 8. Get the 5 posts with the longest-running comment threads
 --    (longest time between first and last comments).
-SELECT posts
+SELECT posts.title, (MAX(comments.created_at) - MIN(comments.created_at)) AS runtime
   FROM posts, comments
   WHERE comments.post_id = posts.id
-  GROUP BY posts
-  ORDER BY (MAX(comments.created_at) - MIN(comments.created_at)) DESC LIMIT 5;
+  GROUP BY posts.title
+  ORDER BY runtime DESC LIMIT 5;
 
 -- 9. Get all posts by a specific user that have comments,
 --    but which that user hasn't commented on.
-SELECT posts
+SELECT DISTINCT posts.title
   FROM posts, comments
   WHERE comments.post_id = posts.id AND comments.author_id != posts.author_id AND posts.author_id = 13;
 
 -- 10. Find which category of post each user has made the most comments on.
-SELECT users.login, MAX(categories.name)
+SELECT users.login, MAX(categories.name) AS most_commented_category
   FROM users, comments, posts, categories
   WHERE users.id = comments.author_id AND comments.post_id = posts.id AND posts.category_id = categories.id
-  GROUP BY users.login, categories.name;
+  GROUP BY users.login;
