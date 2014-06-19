3b1a8201f0cb89e196bd4b9bdd54e08ba4a3d05e
csb324 <csb324@gmail.com>
2014-06-19 17:33:52 -0400

    Questions 4 and 5!

--- queries.sql
+++ queries.sql
@@ -9,9 +9,19 @@ SELECT * FROM posts ORDER BY EXTRACT(year FROM created_at);
 SELECT * FROM posts ORDER BY LENGTH(content) DESC LIMIT 5;
 
 -- 4. Find how many comments each user has across all of their posts.
-
+SELECT users.id, users.login, count(comments)
+  FROM users, comments, posts
+  WHERE users.id = posts.author_id AND comments.post_id = posts.id
+  GROUP BY users.id
+  ORDER BY count(comments) DESC;
 
 -- 5. Get a specific user's posts sorted by date of most recent comment.
+SELECT users.login, posts.title, comments.content, comments.created_at
+  FROM users, comments, posts
+  WHERE posts.author_id = users.id
+  AND comments.post_id = posts.id
+  AND users.login = 'George'
+  ORDER BY comments.created_at DESC;
 
 
 -- 6. Find how many comments each user has made per post category.
